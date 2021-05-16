4b822bb5af7d0964bc50412a2900a707004c077d
GeJavaGoHuai <529118861@qq.com>
2021-05-16 20:34:15 +0800

    first commit

---
+++ src/ModulusOperation.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ModulusOperation {
+    @Test
+    public void modulusoperation() {
+
+        System.out.println((1200338437 % 8));
+        System.out.println((1200338437 % 128));
+    }
+}
